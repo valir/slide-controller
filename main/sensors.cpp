6d9a653b7949d3fa4e3a5a60d14f7b429e4ed04b
Valentin Rusu <valir@rusu.info>
2022-09-15 11:32:59 +0300

    Switch to I2C

--- main/sensors.cpp
+++ main/sensors.cpp
@@ -2,17 +2,18 @@
 #include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <driver/spi_master.h>
+#include <driver/i2c.h>
 #include <driver/gpio.h>
 
 #include "display.h"
 #include "events.h"
 #include "sensors.h"
 
-#define PIN_NUM_MISO 19
-#define PIN_NUM_MOSI 23
-#define PIN_NUM_CLK 18
-#define PIN_NUM_CS 33
+#define PIN_NUM_SDI 26
+#define PIN_NUM_CLK 25
+#define PIN_NUM_CS  33
+#define I2C_MASTER_NUM 0
+#define I2C_ADDR 0x77 // BME680 address is hardcoded
 
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 
@@ -24,31 +25,8 @@ extern TaskHandle_t displayTaskHandle;
 
 Sensors_Info sensors_info;
 
-spi_device_handle_t bme_dev;
-
-class BME680_ReadTransaction {
-  public:
-    BME680_ReadTransaction(uint8_t register_address) {
-      if (0x80 & register_address) {
-      // TODO perform automatic page change command
-      }
-      _trans.tx_data[0] = 0x80 | register_address;
-    }
-  protected:
-    void execute() {
-      _trans.flags = SPI_TRANS_USE_TXDATA;
-      _trans.cmd = 1;
-      _trans.addr = 7;
-      _trans.length = 0;
-      _trans.rxlength = 16;
-      ESP_ERROR_CHECK(spi_device_polling_transmit(bme_dev, &_trans));
-    }
-  private:
-    spi_transaction_t _trans;
-};
-
-class BME680_WriteTransaction {
-};
+void bme680_init() {
+}
 
 void select_oversampling() {
 }
@@ -95,32 +73,19 @@ void sensors_task(void*)
   vTaskDelay(pdMS_TO_TICKS(1*1000)); // wait for other initializations to take
                                      // place
 
-  ESP_LOGI(TAG, "Initializing bus SPI3");
-  spi_bus_config_t buscfg = {
-    .mosi_io_num = PIN_NUM_MOSI,
-    .miso_io_num = PIN_NUM_MISO,
-    .sclk_io_num = PIN_NUM_CLK,
-    .quadwp_io_num = -1,
-    .quadhd_io_num = -1,
-    .data4_io_num = -1,
-    .data5_io_num = -1,
-    .data6_io_num = -1,
-    .data7_io_num = -1,
-    .max_transfer_sz = 32
+  ESP_LOGI(TAG, "Initializing I2C");
+  i2c_config_t i2c_conf = {
+    .mode = I2C_MODE_MASTER,
+    .sda_io_num = PIN_NUM_SDI,
+    .scl_io_num = PIN_NUM_CLK,
+    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+    .scl_pullup_en = GPIO_PULLUP_ENABLE,
   };
-  ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO));
+  i2c_conf.master.clk_speed = 100000;
+  ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_NUM, &i2c_conf));
+  ESP_ERROR_CHECK(i2c_driver_install( I2C_MASTER_NUM, i2c_conf.mode, 0, 0, 0 ));
 
-  spi_device_interface_config_t bmecfg = {
-    .command_bits = 1, // command is READ or WRITE
-    .address_bits = 7,
-    .mode = 0,
-    .clock_speed_hz = (1*1000*1000), // 1MHz even though chip supports 10Mhz
-    .input_delay_ns = 40,
-    .spics_io_num = PIN_NUM_CS,
-    .flags = SPI_DEVICE_HALFDUPLEX,
-    .queue_size = 1,
-  };
-  ESP_ERROR_CHECK(spi_bus_add_device(SPI3_HOST, &bmecfg, &bme_dev));
+  bme680_init();
 
   // retrieve any previously saved state from MQTT before proceeding
   restore_sensors_state();
@@ -141,7 +106,7 @@ void sensors_task(void*)
     .skip_unhandled_events = 1 };
   esp_timer_handle_t backup_timer_handle;
   ESP_ERROR_CHECK(esp_timer_create(&backup_timer_args, &backup_timer_handle));
-  ESP_ERROR_CHECK(esp_timer_start_periodic(backup_timer_handle, 3600*1000*1000));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(backup_timer_handle, 3600*1000*1000ul));
 
   vTaskDelete(nullptr);
 }
