d433af5758e7d8e03dff4483cc63669777c646d3
Valentin Rusu <valir@rusu.info>
2023-01-13 00:14:27 +0200

    Display version number

--- main/statusbar.cpp
+++ main/statusbar.cpp
@@ -2,13 +2,23 @@
 #include "statusbar.h"
 #include <esp_wifi.h>
 
-lv_obj_t *time_label = nullptr;
-lv_obj_t *status_label = nullptr;
+lv_obj_t* time_label = nullptr;
+lv_obj_t* status_label = nullptr;
+lv_obj_t* version_label = nullptr;
 
-StatusBar::StatusBar(lv_obj_t *parent) {
+#ifndef SC_VERSION
+#error SC_VERSION is not defined
+#endif
+
+#define STR(n) #n
+#define VERSION_AS_TEXT(n) STR(n)
+
+StatusBar::StatusBar(lv_obj_t* parent)
+{
   static lv_style_t status_style;
   lv_style_init(&status_style);
-  lv_style_set_text_color(&status_style, lv_palette_darken(LV_PALETTE_AMBER, 4));
+  lv_style_set_text_color(
+      &status_style, lv_palette_darken(LV_PALETTE_AMBER, 4));
   lv_style_set_text_font(&status_style, &lv_font_montserrat_16);
 
   time_label = lv_label_create(parent);
@@ -25,34 +35,41 @@ StatusBar::StatusBar(lv_obj_t *parent) {
   lv_obj_align(status_label, LV_ALIGN_TOP_RIGHT, 0, 0);
   lv_obj_add_flag(status_label, LV_OBJ_FLAG_HIDDEN);
 
+  version_label = lv_label_create(parent);
+  lv_obj_add_style(version_label, &status_style, 0);
+  lv_label_set_long_mode(version_label, LV_LABEL_LONG_CLIP);
+  lv_label_set_text(version_label, VERSION_AS_TEXT(SC_VERSION));
+  lv_obj_set_style_text_align(version_label, LV_TEXT_ALIGN_LEFT, 0);
+  lv_obj_align(version_label, LV_ALIGN_BOTTOM_LEFT, 0, 0);
 }
 
-
-int8_t getWifiQuality() {
+int8_t getWifiQuality()
+{
   wifi_ap_record_t ap_info;
   ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&ap_info));
   auto dbm = ap_info.rssi;
-  if(dbm <= -100) {
-      return 0;
-  } else if(dbm >= -50) {
-      return 100;
+  if (dbm <= -100) {
+    return 0;
+  } else if (dbm >= -50) {
+    return 100;
   } else {
-      return 2 * (dbm + 100);
+    return 2 * (dbm + 100);
   }
 }
 
-void StatusBar::update() {
+void StatusBar::update()
+{
   static bool firstUpdate = true;
   time_t now = ::time(nullptr);
-  struct tm * timeinfo = localtime(&now);
-  lv_label_set_text_fmt(time_label, "%2d:%02d",timeinfo->tm_hour, timeinfo->tm_min);
+  struct tm* timeinfo = localtime(&now);
+  lv_label_set_text_fmt(
+      time_label, "%2d:%02d", timeinfo->tm_hour, timeinfo->tm_min);
 
-  lv_label_set_text_fmt(status_label, LV_SYMBOL_WIFI " %d%% ",
-      getWifiQuality());
+  lv_label_set_text_fmt(
+      status_label, LV_SYMBOL_WIFI " %d%% ", getWifiQuality());
 
   if (firstUpdate) {
     lv_obj_clear_flag(time_label, LV_OBJ_FLAG_HIDDEN);
     lv_obj_clear_flag(status_label, LV_OBJ_FLAG_HIDDEN);
   }
-
 }
